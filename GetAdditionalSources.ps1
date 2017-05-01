62f6202d68b109cb999be92e7a80d64240fabe0a
Tim LaJaunie <TLaJaunie@cs.local>
2017-05-01 08:56:55 -0500

    Initial commit

---
+++ GetAdditionalSources.ps1
@@ -0,0 +1,71 @@
+﻿param(
+   [string]$source,
+   [string]$destination
+)
+
+Write-Host "Source: $source"
+Write-Host "Destination: $destination"
+
+Write-Host "TFS Server: $env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI"
+
+$teamExplorerPath = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\*\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\'
+
+Get-ChildItem -Path ($teamExplorerPath + "*") -Filter *.dll -File | ForEach-Object {
+    try { [void][System.Reflection.Assembly]::LoadFrom($_.FullName) } catch { }
+}
+
+$credentials = New-Object Microsoft.TeamFoundation.Client.TfsClientCredentials -ArgumentList @((New-Object Microsoft.TeamFoundation.Client.WindowsCredential), $false)
+
+$collection = New-Object Microsoft.TeamFoundation.Client.TfsTeamProjectCollection -ArgumentList @($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI, $credentials)
+
+$sourceControl = [Microsoft.TeamFoundation.VersionControl.Client.VersionControlServer]$collection.GetService([Microsoft.TeamFoundation.VersionControl.Client.VersionControlServer])
+
+$changeset = $env:BUILD_SOURCEVERSION
+
+if ($changeset.StartsWith("C")) {
+    $changeset = $changeset.Substring(1)
+}
+
+$versionSpec = New-Object Microsoft.TeamFoundation.VersionControl.Client.ChangesetVersionSpec -ArgumentList $changeset
+
+if ([IO.Path]::IsPathRooted($destination)) {
+    $qualifiedDestination = $destination
+}
+else {
+    $qualifiedDestination = Join-Path $env:Build_SourcesDirectory $destination
+}
+
+Write-Host "Qualified Destination: $qualifiedDestination"
+
+$sourceItem = $sourceControl.GetItem($source, $versionSpec)
+$sourceItemType = $sourceItem.ItemType
+
+Write-Host $sourceItem
+
+$recursionType = [Microsoft.TeamFoundation.VersionControl.Client.RecursionType]::None
+if ($sourceItemType -ieq 'folder') {
+    $recursionType = [Microsoft.TeamFoundation.VersionControl.Client.RecursionType]::Full
+    
+}
+
+$itemSet = $sourceControl.GetItems($source, $versionSpec, $recursionType)
+
+$itemSet.Items | ForEach-Object {
+    $item = $_
+    $itemServerItem = $item.ServerItem
+
+    $itemDestination = $item.ServerItem.Replace($source, $qualifiedDestination)
+
+    if ($item.ItemType -ieq 'folder') {
+        [IO.Directory]::CreateDirectory($itemDestination) | Out-Null
+    }
+    elseif ($item.ItemType -ieq 'file') {
+        Write-Host "Getting $itemServerItem"
+
+        $parentDirectory = [IO.Path]::GetDirectoryName($itemDestination)
+
+        [IO.Directory]::CreateDirectory($parentDirectory) | Out-Null
+
+        $sourceControl. DownloadFile($item.ServerItem, $itemDestination)
+    }
+}
---
+++ get-additional-sources.ts
@@ -0,0 +1,58 @@
+import * as Q from 'q'
+import * as cm from './common'
+import * as vm from 'vso-node-api'
+import * as vc from 'vso-node-api/TfvcApi'
+import * as vci from "vso-node-api/interfaces/TfvcInterfaces"
+import * as tasklib from 'vsts-task-lib'
+import * as path from 'path'
+import { IRequestHandler } from "vso-node-api/interfaces/common/VsoBaseInterfaces"
+
+const endpointAuthorizationKey = "SYSTEMVSSCONNECTION"
+
+async function run() {
+
+    let endpointAuth: tasklib.EndpointAuthorization = tasklib.getEndpointAuthorization(endpointAuthorizationKey, false)
+
+    let token: string = endpointAuth.parameters["AccessToken"]
+
+    let authHandler: IRequestHandler = vm.getPersonalAccessTokenHandler(token)
+
+    let collectionUri: string = tasklib.getVariable("System.TeamFoundationCollectionUri")
+
+    let vsts: vm.WebApi = new vm.WebApi(collectionUri, authHandler)
+
+    let tfvc: vc.ITfvcApi = vsts.getTfvcApi()
+
+    let sourceVersion: string = tasklib.getVariable("Build.SourceVersion")
+
+    if (sourceVersion.substr(0, 1) == "C")
+        sourceVersion = sourceVersion.substring(1);
+
+    let versionDescriptor: vci.TfvcVersionDescriptor = {
+        version: sourceVersion,
+        versionOption: vci.TfvcVersionOption.None,
+        versionType: vci.TfvcVersionType.Changeset
+    }
+
+    let source = tasklib.getInput("Source")
+
+    let sourcesDirectory = tasklib.getVariable('Build.SourcesDirectory')
+
+    let destination = path.join(sourcesDirectory, tasklib.getInput("Destination"))
+
+    let project: string = tasklib.getVariable("System.TeamProject")
+
+    await tfvc.getItem(source, project).then(async item => {
+
+        let recursionLevel: vci.VersionControlRecursionType
+
+        if (item.isFolder)
+            recursionLevel = vci.VersionControlRecursionType.Full
+        else
+            recursionLevel = vci.VersionControlRecursionType.None
+
+        await tfvc.getItem(source, project, undefined, true, destination, recursionLevel, versionDescriptor)
+    })
+}
+
+run()
